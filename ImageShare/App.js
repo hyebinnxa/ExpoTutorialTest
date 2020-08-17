22d645c48c37a55e57bc3ef1b3d992c449d06512
hyebinnxa <hyebinnxa@gmail.com>
2020-08-18 04:02:33 +0900

    Displaying the image in the app using the Image component.

--- ImageShare/App.js
+++ ImageShare/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* Image component는 명확하게 width,height를 명시해줘야 한다. 일반적으로 RN에서 사진 width:305 */}
+      <Image source={logo} style={{width:305, height:159}}/>
+
       <Text style={{color: '#888', fontSize: 18}}> 
       To share a photo from your phone with a friend, just press the button below!
       </Text>
