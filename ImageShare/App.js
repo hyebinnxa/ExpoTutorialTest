ae95744b07335cc2fcc8f32f4c3930884688bfa2
hyebinnxa <hyebinnxa@gmail.com>
2020-08-18 04:07:04 +0900

    Organizing code a bit by moving styles into one place

--- ImageShare/App.js
+++ ImageShare/App.js
@@ -7,9 +7,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       {/* Image component는 명확하게 width,height를 명시해줘야 한다. 일반적으로 RN에서 사진 width:305 */}
-      <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={{width:305, height:159}}/>
+      <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={styles.logo}/>
 
-      <Text style={{color: '#888', fontSize: 18}}> 
+      <Text style={styles.instructions}> 
       To share a photo from your phone with a friend, just press the button below!
       </Text>
       <StatusBar style="auto" />
@@ -23,5 +23,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305, 
+    height: 159,
+    // space things out between the logo and the instructions
+    marginBottom: 10,
+  },
+  instructions: {
+    color:'#888', 
+    fontSize:18,
+    // space around the edges of the screen
+    marginHorizontal: 15,
   },
 });
